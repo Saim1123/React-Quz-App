57a849d27d75b106e16b38daa3e4b39356611bcc
saim1123 <saimtahir3211@gmail.com>
2023-12-04 12:42:28 +0500

    first commit

---
+++ src/components/Start.jsx
@@ -0,0 +1,25 @@
+import React, { useRef } from "react";
+
+const Start = ({ setUserName }) => {
+  const inputRef = useRef();
+
+  const handleClick = () => {
+    inputRef.current.value && setUserName(inputRef.current.value);
+  };
+
+  return (
+    <div className="start">
+      <input
+        type="text "
+        placeholder="enter your name"
+        className="startInput"
+        ref={inputRef}
+      />
+      <button className="startButton" onClick={handleClick}>
+        Start
+      </button>
+    </div>
+  );
+};
+
+export default Start;
